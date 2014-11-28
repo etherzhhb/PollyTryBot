7522bfc26914b127f6421f560fd969af004f4d16
ether <etherzhhb@gmail.com>
2014-11-28 14:27:54 +0100

    Final patch to enable try perf slaves

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -4,20 +4,70 @@ from zorg.buildbot.builders import PollyBuilder
 
 
 def get_builders():
-    yield {'name': "polly-intel32-linux",
-           'category' : "polly",
-           'slavenames':["botether"],
-           'builddir':"polly-intel32-linux",
-           'factory': PollyBuilder.getPollyBuildFactory()}
-
-    yield  {'name': "perf-i386-penryn-O3-polly-detect-only",
-         'slavenames':["botether"],
+    return [
+        {'name': "polly-amd64-linux",
+         'slavenames':["parkas1_bot_ether"],
+         'builddir':"polly-amd64-linux",
          'category' : "polly",
+         'factory': PollyBuilder.getPollyBuildFactory()},
+
+        {'name': "perf-x86_64-penryn-O3-polly-fast",
+         'slavenames':["parkas1_bot_ether"],
+         'builddir': "perf-x86_64-penryn-O3-polly-fast",
+         'category' : "polly",
+         'factory': PollyBuilder.getPollyLNTFactory(triple="x86_64-pc-linux-gnu",
+                                                    nt_flags=['--multisample=1', '--mllvm=-polly', '-j16' ],
+                                                    reportBuildslave=False,
+                                                    package_cache="http://parkas1.inria.fr/packages",
+                                                    submitURL='http://llvm.org/perf/submitRun',
+                                                    testerName='x86_64-penryn-O3-polly-fast')},
+
+        {'name': "perf-x86_64-penryn-O3-polly-parallel-fast",
+         'slavenames':["parkas1_bot_ether"],
+         'builddir': "perf-x86_64-penryn-O3-polly-parallel-fast",
+         'category' : "polly",
+         'factory': PollyBuilder.getPollyLNTFactory(triple="x86_64-pc-linux-gnu",
+                                                    nt_flags=['--multisample=1', '--mllvm=-polly', '--mllvm=-polly-parallel', '-j16', '--cflag=-lgomp' ],
+                                                    reportBuildslave=False,
+                                                    package_cache="http://parkas1.inria.fr/packages",
+                                                    submitURL='http://llvm.org/perf/submitRun',
+                                                    testerName='x86_64-penryn-O3-polly-parallel-fast')},
+
+        {'name': "perf-x86_64-penryn-O3-polly-detect-only",
+         'slavenames':["parkas1_bot_ether"],
          'builddir':"perf-x86_64-penryn-O3-polly-detect-only",
+         'category' : "polly",
          'factory': PollyBuilder.getPollyLNTFactory(triple="x86_64-pc-linux-gnu",
                                                     nt_flags=['--multisample=10', '--mllvm=-polly', '--mllvm=-polly-code-generator=none', '--mllvm=-polly-optimizer=none', '--mllvm=-polly-run-dce=false', '--rerun'],
                                                     reportBuildslave=False,
                                                     package_cache="http://parkas1.inria.fr/packages",
-                                                    submitURL='http://supper.bad.url',
-                                                    testerName='x86_64-penryn-O3-polly-detect-only')}
+                                                    submitURL='http://llvm.org/perf/submitRun',
+                                                    testerName='x86_64-penryn-O3-polly-detect-only')},
+
+        {'name': "perf-x86_64-penryn-O3-polly-detect-and-dependences-only",
+         'slavenames':["parkas1_bot_ether"],
+         'builddir':"perf-x86_64-penryn-O3-polly-detect-and-dependences-only",
+         'category' : "polly",
+         'factory': PollyBuilder.getPollyLNTFactory(triple="x86_64-pc-linux-gnu",
+                                                    nt_flags=['--multisample=10',
+                                                              '--mllvm=-polly',
+                                                              '--mllvm=-polly-optimizer=none',
+                                                              '--mllvm=-polly-code-generator=none',
+                                                              '--rerun'],
+                                                    reportBuildslave=False,
+                                                    package_cache="http://parkas1.inria.fr/packages",
+                                                    submitURL='http://llvm.org/perf/submitRun',
+                                                    testerName='x86_64-penryn-O3-polly-detect-and-dependences-only')},
+
+        {'name': "perf-x86_64-penryn-O3-polly",
+         'slavenames':["parkas1_bot_ether"],
+         'builddir':"perf-x86_64-penryn-O3-polly",
+         'category' : "polly",
+         'factory': PollyBuilder.getPollyLNTFactory(triple="x86_64-pc-linux-gnu",
+                                                    nt_flags=['--multisample=10', '--mllvm=-polly', '--rerun'],
+                                                    reportBuildslave=False,
+                                                    package_cache="http://parkas1.inria.fr/packages",
+                                                    submitURL='http://llvm.org/perf/submitRun',
+                                                    testerName='x86_64-penryn-O3-polly')}
+    ]
 
