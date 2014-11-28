4a074ad0e36ac2c913e052c1f54c3fd2800dd148
ether <etherzhhb@gmail.com>
2014-11-28 20:47:00 +0800

    Add LNT scheduler

--- buildbot/osuosl/master/config/builders.py
+++ buildbot/osuosl/master/config/builders.py
@@ -10,3 +10,14 @@ def get_builders():
            'builddir':"polly-intel32-linux",
            'factory': PollyBuilder.getPollyBuildFactory()}
 
+    yield  {'name': "perf-i386-penryn-O3-polly-detect-only",
+         'slavenames':["botether"],
+         'category' : "polly",
+         'builddir':"perf-x86_64-penryn-O3-polly-detect-only",
+         'factory': PollyBuilder.getPollyLNTFactory(triple="x86_64-pc-linux-gnu",
+                                                    nt_flags=['--multisample=10', '--mllvm=-polly', '--mllvm=-polly-code-generator=none', '--mllvm=-polly-optimizer=none', '--mllvm=-polly-run-dce=false', '--rerun'],
+                                                    reportBuildslave=False,
+                                                    package_cache="http://parkas1.inria.fr/packages",
+                                                    submitURL='http://supper.bad.url',
+                                                    testerName='x86_64-penryn-O3-polly-detect-only')}
+
