779e915fa4825a953293f1e4f09e685ef9fcd259
ether <ether@gcc45.fsffrance.org>
2014-12-03 08:55:25 +0100

    Add parkas 2 and 3

--- buildbot/osuosl/master/config/slaves.py
+++ buildbot/osuosl/master/config/slaves.py
@@ -10,8 +10,12 @@ def create_slave(name, *args, **kwargs):
 
 def get_build_slaves():
     return [
-        # Polly perf servers
+        # parkas bots 
         # Each is a:
         # 8 x Intel(R) Xeon(R) CPU E5430  @ 2.66GHz, Debian x86_64 GNU/Linux
-        create_slave("parkas1_bot_ether", properties={'jobs': 8}, max_builds=1)
+        create_slave("parkas1_bot_ether", properties={'jobs': 8}, max_builds=1),
+
+        create_slave("parkas2", properties={'jobs': 8}, max_builds=1),
+
+        create_slave("parkas3", properties={'jobs': 8}, max_builds=1) 
     ]
