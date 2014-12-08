1a86748c9af800efe451d67adc4e752de92eb695
ether <etherzhhb@gmail.com>
2014-12-08 15:16:01 +0800

    Add gcc slaves

--- buildbot/osuosl/master/config/slaves.py
+++ buildbot/osuosl/master/config/slaves.py
@@ -10,10 +10,23 @@ def create_slave(name, *args, **kwargs):
 
 def get_build_slaves():
     return [
+        # gcc farm bots, for light weight regression test only
+        # gcc10: 2x12x1.5 GHz AMD Opteron Magny-Cours / 64 GB RAM / Supermicro AS-1022G-BTF / Debian x86-64
+        create_slave("gcc10", properties={'jobs': 24}, max_builds=1),
+
+        # gcc14: 2x4x3.0 GHz Intel Xeon X5450 / 16GB RAM / Dell Poweredge 1950 / Debian x86-64
+        create_slave("gcc14", properties={'jobs': 8}, max_builds=1),
+
+        # gcc16: 2x4x2.2 GHz AMD Opteron 8354 (Barcelona B3) / 16 GB RAM / Debian x86-64
+        create_slave("gcc16", properties={'jobs': 8}, max_builds=1),
+
+        # gcc20: 2x6x2.93 GHz Intel Dual Xeon X5670 2.93 GHz 12 cores 24 threads / 24 GB RAM / Debian amd64
+        create_slave("gcc20", properties={'jobs': 12}, max_builds=1),
+        
         # parkas bots 
         # Each is a:
         # 8 x Intel(R) Xeon(R) CPU E5430  @ 2.66GHz, Debian x86_64 GNU/Linux
-        create_slave("parkas1_bot_ether", properties={'jobs': 8}, max_builds=1),
+        create_slave("parkas1", properties={'jobs': 8}, max_builds=1),
 
         create_slave("parkas2", properties={'jobs': 8}, max_builds=1),
 
